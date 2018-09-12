cd8bca62d8641317a82fbf709315bc127821c01f
Curtis LayCraft <curtis.laycraft@zoho.com>
2018-09-12 11:11:36 -0700

    update API page

--- app/server.py
+++ app/server.py
@@ -3,10 +3,13 @@ from storage_handler import StorageHandler
 
 storage = StorageHandler()
 def startServer():
-
 	@error(404)
 	def error404(error):
 		return static_file('api.html', root='app/static')
+
+	@route('/time')
+	def getUpdateTime():
+		return storage.getUpdateTime()
 
 	@route('/list')
 	def getMasterList():
@@ -15,10 +18,6 @@ def startServer():
 	@route('/get/<mac>')
 	def getDevice(mac):
 		return storage.getDevice(mac)
-	
-	@route('/remove/<mac>')
-	def removeDevice(mac):
-		return storage.removeDevice(mac)
 	
 	@route('/reset')
 	def resetSystem(mac):
@@ -29,4 +28,4 @@ def startServer():
 		storage.setDevice(mac, ip, name, location)
 		return storage.setDevice(mac, ip, name, location)
 
-	run(reloader=True, host='localhost', port=8080)
+	run(reloader=True, host='localhost', port=1337)
--- app/storage_handler.py
+++ app/storage_handler.py
@@ -1,12 +1,14 @@
 # this is supposed to be a class that accesses and sets the current information about the IPs it only runs in RAM and doesn't keep records.
 
 from typing import Dict
+from datetime import datetime
 
 class StorageHandler:
 	# Should be a collection of devices in a dictionary. The key for the devices is the mac address
 	# The mac address is used because theoretically a machine could use two IP addresses if it has two network cards
 
 	collection: dict = {}
+	update_time = datetime.now()
 
 	def __init__(self):
 		print("RAM storage initialized. Neat")
@@ -15,10 +17,9 @@ class StorageHandler:
 		# remove the device at the mac address from the dictionary
 		print("Device deleted from master collection")
 		del self.collection[mac]
+		# record the time that the data is changed
+		self.update_time = datetime.now()
 		return self.collection
-	
-	def getDevice(self, mac:str):
-		return self.collection[mac]
 	
 	def setDevice(self, mac:str, ip:str, name:str, location:str):
 		device = {}
@@ -27,11 +28,18 @@ class StorageHandler:
 		device["name"] = name
 		device["location"] = location
 		self.collection[device["MAC"]] = device
+		# record the time that the data is changed
+		self.update_time = datetime.now()
 		return self.collection
 	
 	def getMasterList(self):
 		return self.collection
 
+	def getUpdateTime(self):
+		return str(self.update_time)
+
 	def reset(self):
 		self.collection = {}
+		# record the time that the data is changed
+		self.update_time = datetime.now()
 		return self.collection
