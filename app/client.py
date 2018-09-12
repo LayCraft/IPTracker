5c55fc00034ece7708995f551dd2fe6bccd3f0bb
Curtis LayCraft <curtis.laycraft@zoho.com>
2018-09-12 12:10:13 -0700

    client set info success

--- app/client.py
+++ app/client.py
@@ -1,9 +1,16 @@
+# this client checks to see if the current system information is up to date or if there has been a change to the network. If there is a change it will request a fresh copy of the list and submit a change if the machine's information is out-of-date.
+import urllib.request
+import json
+
 SERVER = "http://localhost"
 PORT = 1337
 
-def sendInfo(mac, ip, name, location):
-	print(mac)
+def setInfo(mac, ip, name, location):
+	# location = SERVER + ':' + str(PORT) + '/set/' + mac + '/' + ip + '/' + name + '/' + location
+	location = "%s:%i/set/%s/%s/%s/%s" % (SERVER, PORT, mac, ip, name, location)
+	# read from the url to submit the data and decode its contents because the contents is a list of all devices.
+	return urllib.request.urlopen(location).read().decode()
+
 
 def startClient():
-	print("Client placeholder")
-	sendInfo("foo", "bar", "baz", "qux")
+	print(setInfo("foo", "bar", "baz", "qux"))
