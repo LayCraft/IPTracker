8407add43b18943d1d6aa58cf664c2f8ee9e0c91
Curtis LayCraft <curtis.laycraft@zoho.com>
2018-09-12 12:18:51 -0700

    returns time

--- app/client.py
+++ app/client.py
@@ -1,20 +1,28 @@
 # this client checks to see if the current system information is up to date or if there has been a change to the network. If there is a change it will request a fresh copy of the list and submit a change if the machine's information is out-of-date.
 import urllib.request
-import json
 
 SERVER = "localhost"
 PORT = 1337
 
+# Build once and use many times. Reduces string concat later.
+BASE_URL = "http://%s:%i" % (SERVER, PORT)
+
 def setInfo(mac, ip, name, location):
-	url = "http://%s:%i/set/%s/%s/%s/%s" % (SERVER, PORT, mac, ip, name, location)
+	url = "%s/set/%s/%s/%s/%s" % (BASE_URL, mac, ip, name, location)
 	# read from the url to submit the data and decode its contents because the contents is a list of all devices.
 	return urllib.request.urlopen(url).read().decode()
 
 def getInfo():
-	url = "http://%s:%i/get" % (SERVER, PORT)
+	url = "%s/get" % (BASE_URL)
 	return urllib.request.urlopen(url).read().decode()
+
+def getTime():
+	url = "%s/time" % (BASE_URL)
+	return urllib.request.urlopen(url).read().decode()
+
 
 
 def startClient():
 	print(setInfo("foo", "bar", "baz", "qux"))
 	print(getInfo())
+	print(getTime())
