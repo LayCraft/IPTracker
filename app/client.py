56559b77247df1cdfeb320977db68910961129e5
Curtis LayCraft <curtis.laycraft@zoho.com>
2018-09-12 15:57:48 -0700

    read from static file

--- app/client.py
+++ app/client.py
@@ -3,9 +3,13 @@ import urllib.request
 import platform
 import os
 import sys
+import netifaces
 
 SERVER = "localhost"
 PORT = 1337
+
+# this collection of tuples is a place to put results
+identifiers: list = []
 
 # Build once and use many times. Reduces string concat later.
 BASE_URL = "http://%s:%i" % (SERVER, PORT)
@@ -29,11 +33,58 @@ def removeInfo(mac):
 
 def getMachineInfo():
 	# determine OS
-	print(os.name) # should always return "nt" or "posix"
-
+	info = getNtInfo()
+	# print(os.name) # should always return "nt" or "posix"
+	# print(netifaces.interfaces())
+	# print(netifaces.ifaddresses('{6B1D5DE4-5CA4-49E3-BFFE-8F2D390E9E0E}'))
 	# query the machine the right way
 	# return platform.system()
-	return None
+	return info
+
+def getNtInfo():
+	# get the machine's location from the static file
+	f = open('{0}{1}static{1}location.txt'.format(os.path.dirname(os.path.realpath(__file__)), os.path.sep), 'r')
+	location = f.readline()
+	f.close()
+
+	# it is named like this because the os name is nt if it is running a version of windows.
+	interfaces = netifaces.interfaces()
+	#print(interfaces) # make an ugly array of interface IDs ['{6B1D5DE4-5CA4-49E3-BFFE-8F2D390E9E0E}', '{9739C6A0-FFFC-4300-866C-499268613232}', '{199049E3-808B-40FE-811B-524006C5DB71}', '{EA7B8442-B8CB-40F9-8503-BE17E1934E6B}', '{F1D5BADB-4A4B-11E8-AF18-806E6F6E6963}']
+	for interface in interfaces:
+		# make a list of 4 emptystring elements
+		collector = ['']*4
+
+		# copy the mac address into the first slot
+		collector[0] = netifaces.ifaddresses(interface)[netifaces.AF_LINK][0]['addr']
+		# print(netifaces.ifaddresses(interface))
+			# If the mac address is blank then move on. MAC is a primary key so is mandatory
+		if collector[0] == '':
+			continue
+		
+		# collect the IP addresses
+		if 23 in netifaces.ifaddresses(interface):
+			# there is an ipv6 address so we copy the ipv6 first and let the next part overwrite the ipv6 if it exists.
+			collector[1] = netifaces.ifaddresses(interface)[23][0]['addr']
+		# if there is the ipv4 element and it isn't blank then we overwrite the ipv6 address
+		if 2 in netifaces.ifaddresses(interface) and netifaces.ifaddresses(interface)[2][0]['addr'] != '':
+			collector[1] = netifaces.ifaddresses(interface)[2][0]['addr']
+		
+		'''
+		# Three ways to get your computer name. Saving for linux use later. All work in nt.
+		platform.node()
+		socket.gethostname()
+		os.environ['COMPUTERNAME']
+		'''
+
+		# save the machine name
+		collector[2] = platform.node()
+		collector[3] = location
+
+		# save the collector into it
+		identifiers.append(collector)
+		# print(collector)
+	return identifiers
+
 
 
 def startClient():
